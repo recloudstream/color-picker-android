bc9a9c2452fd83317f9a0636fe4638edd5ae448d
Sagi <33753500+sagishporer@users.noreply.github.com>
2024-07-08 09:15:48 +0300

    Auto indentation

--- demo/src/main/java/com/jaredrummler/android/colorpicker/demo/ColorPickerActivity.java
+++ demo/src/main/java/com/jaredrummler/android/colorpicker/demo/ColorPickerActivity.java
@@ -19,60 +19,67 @@ package com.jaredrummler.android.colorpicker.demo;
 import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+
 import androidx.preference.PreferenceManager;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.jaredrummler.android.colorpicker.ColorPanelView;
 import com.jaredrummler.android.colorpicker.ColorPickerView;
 import com.jaredrummler.android.colorpicker.ColorPickerView.OnColorChangedListener;
 
 public class ColorPickerActivity extends AppCompatActivity implements OnColorChangedListener, View.OnClickListener {
 
-  private ColorPickerView colorPickerView;
-  private ColorPanelView newColorPanelView;
+    private ColorPickerView colorPickerView;
+    private ColorPanelView newColorPanelView;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().setFormat(PixelFormat.RGBA_8888);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFormat(PixelFormat.RGBA_8888);
 
-    setContentView(R.layout.activity_color_picker);
+        setContentView(R.layout.activity_color_picker);
 
-    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    int initialColor = prefs.getInt("color_3", 0xFF000000);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int initialColor = prefs.getInt("color_3", 0xFF000000);
 
-    colorPickerView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_picker_view);
-    ColorPanelView colorPanelView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_panel_old);
-    newColorPanelView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_panel_new);
+        colorPickerView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_picker_view);
+        ColorPanelView colorPanelView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_panel_old);
+        newColorPanelView = findViewById(com.jaredrummler.android.colorpicker.R.id.cpv_color_panel_new);
 
-    Button btnOK = findViewById(R.id.okButton);
-    Button btnCancel = findViewById(R.id.cancelButton);
+        Button btnOK = findViewById(R.id.okButton);
+        Button btnCancel = findViewById(R.id.cancelButton);
 
-    ((LinearLayout) colorPanelView.getParent()).setPadding(colorPickerView.getPaddingLeft(), 0,
-        colorPickerView.getPaddingRight(), 0);
+        ((LinearLayout) colorPanelView.getParent()).setPadding(colorPickerView.getPaddingLeft(), 0,
+                colorPickerView.getPaddingRight(), 0);
 
-    colorPickerView.setOnColorChangedListener(this);
-    colorPickerView.setColor(initialColor, true);
-    colorPanelView.setColor(initialColor);
+        colorPickerView.setOnColorChangedListener(this);
+        colorPickerView.setColor(initialColor, true);
+        colorPanelView.setColor(initialColor);
 
-    btnOK.setOnClickListener(this);
-    btnCancel.setOnClickListener(this);
-  }
-
-  @Override public void onColorChanged(int newColor) {
-    newColorPanelView.setColor(colorPickerView.getColor());
-  }
-
-  @Override public void onClick(View v) {
-    int viewId = v.getId();
-    if (viewId == R.id.okButton) {
-      SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(this).edit();
-      edit.putInt("color_3", colorPickerView.getColor());
-      edit.apply();
-      finish();
-    } else if (viewId == R.id.cancelButton) {
-        finish();
+        btnOK.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
     }
-  }
+
+    @Override
+    public void onColorChanged(int newColor) {
+        newColorPanelView.setColor(colorPickerView.getColor());
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.okButton) {
+            SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(this).edit();
+            edit.putInt("color_3", colorPickerView.getColor());
+            edit.apply();
+            finish();
+        } else if (viewId == R.id.cancelButton) {
+            finish();
+        }
+    }
 }
