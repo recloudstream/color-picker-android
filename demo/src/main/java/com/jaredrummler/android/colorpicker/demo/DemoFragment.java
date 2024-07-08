4c25b598476c2417b1f516d4ab2530a21c98375b
Sagi <33753500+sagishporer@users.noreply.github.com>
2024-07-08 09:20:07 +0300

    Auto indentation.

--- demo/src/main/java/com/jaredrummler/android/colorpicker/demo/DemoFragment.java
+++ demo/src/main/java/com/jaredrummler/android/colorpicker/demo/DemoFragment.java
@@ -2,28 +2,30 @@ package com.jaredrummler.android.colorpicker.demo;
 
 import android.os.Bundle;
 import android.util.Log;
+
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.jaredrummler.android.colorpicker.ColorPreferenceCompat;
 
 public class DemoFragment extends PreferenceFragmentCompat {
 
-  private static final String TAG = DemoFragment.class.getSimpleName();
+    private static final String TAG = DemoFragment.class.getSimpleName();
 
-  private static final String KEY_DEFAULT_COLOR = "default_color";
+    private static final String KEY_DEFAULT_COLOR = "default_color";
 
-  @Override public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-    setPreferencesFromResource(R.xml.main, rootKey);
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.main, rootKey);
 
-    // Example showing how we can get the new color when it is changed:
-    ColorPreferenceCompat colorPreference = findPreference(KEY_DEFAULT_COLOR);
-      assert colorPreference != null;
-      colorPreference.setOnPreferenceChangeListener((preference, newValue) -> {
-      if (KEY_DEFAULT_COLOR.equals(preference.getKey())) {
-        String newDefaultColor = Integer.toHexString((int) newValue);
-        Log.d(TAG, "New default color is: #" + newDefaultColor);
-      }
-      return true;
-    });
-  }
+        // Example showing how we can get the new color when it is changed:
+        ColorPreferenceCompat colorPreference = findPreference(KEY_DEFAULT_COLOR);
+        assert colorPreference != null;
+        colorPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+            if (KEY_DEFAULT_COLOR.equals(preference.getKey())) {
+                String newDefaultColor = Integer.toHexString((int) newValue);
+                Log.d(TAG, "New default color is: #" + newDefaultColor);
+            }
+            return true;
+        });
+    }
 }
