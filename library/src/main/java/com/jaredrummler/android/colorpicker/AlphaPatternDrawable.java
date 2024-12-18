ba4ca17deec429a387b8249bb0f0b61cb466f544
Sagi <33753500+sagishporer@users.noreply.github.com>
2024-12-18 07:46:03 +0200

    Performance optimization - draw the AlphaPatternDrawable using a single BitmapShader.

--- library/src/main/java/com/jaredrummler/android/colorpicker/AlphaPatternDrawable.java
+++ library/src/main/java/com/jaredrummler/android/colorpicker/AlphaPatternDrawable.java
@@ -18,11 +18,13 @@ package com.jaredrummler.android.colorpicker;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 
 import androidx.annotation.ColorInt;
@@ -33,38 +35,43 @@ import androidx.annotation.NonNull;
  * It's the pattern you will often see as a background behind a partly transparent image in many applications.
  */
 public class AlphaPatternDrawable extends Drawable {
-
-    private final int rectangleSize;
-
-    private final Paint paint = new Paint();
-    private final Paint paintWhite = new Paint();
-    private final Paint paintGray = new Paint();
-
-    private int numRectanglesHorizontal;
-    private int numRectanglesVertical;
-
-    /**
-     * Bitmap in which the pattern will be cached.
-     * This is so the pattern will not have to be recreated each time draw() gets called.
-     * Because recreating the pattern i rather expensive. I will only be recreated if the size changes.
-     */
-    private Bitmap bitmap;
+    private final Paint patternPaint;
 
     public AlphaPatternDrawable(int rectangleSize) {
         this(rectangleSize, 0xFFFFFFFF, 0xFFCBCBCB);
     }
 
     public AlphaPatternDrawable(int rectangleSize, @ColorInt int color1, @ColorInt int color2) {
-        this.rectangleSize = rectangleSize;
-        paintWhite.setColor(color1);
-        paintGray.setColor(color2);
+        // Create a small bitmap to define the checkerboard pattern
+        Bitmap patternBitmap = Bitmap.createBitmap(rectangleSize * 2, rectangleSize * 2, Config.ARGB_8888);
+        Canvas patternCanvas = new Canvas(patternBitmap);
+
+        Paint paint = new Paint();
+
+        // Draw the checkerboard pattern on the small bitmap
+        Rect rect = new Rect(0, 0, rectangleSize, rectangleSize);
+        paint.setColor(color1);
+        patternCanvas.drawRect(rect, paint);
+
+        rect.offset(rectangleSize, rectangleSize);
+        patternCanvas.drawRect(rect, paint);
+
+        paint.setColor(color2);
+        rect.offset(-rectangleSize, 0);
+        patternCanvas.drawRect(rect, paint);
+
+        rect.offset(rectangleSize, -rectangleSize);
+        patternCanvas.drawRect(rect, paint);
+
+        // Use the small bitmap as a shader for the Paint object
+        BitmapShader shader = new BitmapShader(patternBitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        patternPaint = new Paint();
+        patternPaint.setShader(shader);
     }
 
     @Override
     public void draw(@NonNull Canvas canvas) {
-        if (bitmap != null && !bitmap.isRecycled()) {
-            canvas.drawBitmap(bitmap, null, getBounds(), paint);
-        }
+        canvas.drawPaint(patternPaint);
     }
 
     @Override
@@ -80,44 +87,5 @@ public class AlphaPatternDrawable extends Drawable {
     @Override
     public void setColorFilter(ColorFilter cf) {
         throw new UnsupportedOperationException("ColorFilter is not supported by this drawable.");
-    }
-
-    @Override
-    protected void onBoundsChange(@NonNull Rect bounds) {
-        super.onBoundsChange(bounds);
-        float height = bounds.height();
-        float width = bounds.width();
-        numRectanglesHorizontal = (int) Math.ceil((width / rectangleSize));
-        numRectanglesVertical = (int) Math.ceil(height / rectangleSize);
-        generatePatternBitmap();
-    }
-
-    /**
-     * This will generate a bitmap with the pattern as big as the rectangle we were allow to draw on.
-     * We do this to chache the bitmap so we don't need to recreate it each time draw() is called since it takes a few
-     * milliseconds
-     */
-    private void generatePatternBitmap() {
-        if (getBounds().width() <= 0 || getBounds().height() <= 0) {
-            return;
-        }
-
-        bitmap = Bitmap.createBitmap(getBounds().width(), getBounds().height(), Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-
-        Rect r = new Rect();
-        boolean verticalStartWhite = true;
-        for (int i = 0; i <= numRectanglesVertical; i++) {
-            boolean isWhite = verticalStartWhite;
-            for (int j = 0; j <= numRectanglesHorizontal; j++) {
-                r.top = i * rectangleSize;
-                r.left = j * rectangleSize;
-                r.bottom = r.top + rectangleSize;
-                r.right = r.left + rectangleSize;
-                canvas.drawRect(r, isWhite ? paintWhite : paintGray);
-                isWhite = !isWhite;
-            }
-            verticalStartWhite = !verticalStartWhite;
-        }
     }
 }
